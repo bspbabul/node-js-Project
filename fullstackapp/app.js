845505c79a13728f1019b45f5f3510ad3ebeaf5d
babul <bsp020@nist.edu>
2022-10-04 23:25:07 +0530

    commit1

---
+++ fullstackapp/app.js
@@ -0,0 +1,32 @@
+let express=require('express');
+let app=express()
+let port = process.env.PORT||9700;
+let morgan=require('morgan');
+let categoryRouter=require('./src/controller/categoryRoute')
+let productsRouter=require('./src/controller/productsRoute')
+
+app.use(express.static(__dirname+'/public'));
+
+app.set('views','./src/views');
+
+app.set('view engine','ejs');
+
+
+
+
+
+
+app.get('/',function(req,res){
+    res.render('index',{title:'home page'})
+})
+
+app.use('/category',categoryRouter);
+app.use('/products',productsRouter);
+
+app.listen(port,function(err){
+    if(err)throw err;
+    else{
+        console.log("server is running on port "+port);
+    }
+})
+
